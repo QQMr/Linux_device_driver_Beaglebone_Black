3e82a38bbc0a4960897a4177dd915a88f609e475
Burton <cchangwo@yahoo.com.tw>
2021-04-18 00:38:39 +0800

    add hello world project

---
+++ 001hello_world/main.c
@@ -0,0 +1,23 @@
+#include<linux/module.h>
+#include<linux/fs.h>
+
+static int __init helloworld_init(void)
+{
+	pr_info("Hello world\n");
+	return 0;
+}
+
+
+
+static void __exit helloworld_cleanup(void)
+{
+	pr_info("Good bye world\n");
+}
+
+module_init(helloworld_init);
+module_exit(helloworld_cleanup);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("ME");
+MODULE_DESCRIPTION("A simple hello world kernel module");
+MODULE_INFO(board,"beagle bone wireless");
